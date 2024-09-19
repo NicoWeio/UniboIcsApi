2db6b52ae5e5b2180cad0fa5b11cf32330438017
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2024-09-19 13:41:42 +0200

    fun with timezones (ICS is kinda stupid)

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,32 +1,11 @@
 import base64
-from datetime import datetime
 
-import requests
-from ics import Calendar, Event
-
-
-def parse_date(date_str):
-    return datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S")
+from main import get_cal
 
 
 def lambda_handler(event, context):
-    url = "https://corsi.unibo.it/2cycle/ParticlePhysics/timetable/@@orario_reale_json?anno=2&curricula=&start=2024-09-23&end=2024-09-30"
-    response = requests.get(url)
-    data = response.json()
-
-    cal = Calendar()
-
-    for item in data:
-        event = Event()
-        event.name = item.get('title', 'No Title')
-        event.begin = parse_date(item['start'])
-        event.end = parse_date(item['end'])
-        event.location = item['aule'][0]['des_ubicazione'] if item['aule'] else 'No Location'
-        event.description = f"Professor: {item.get('docente', 'No Professor')}\nNotes: {item.get('note', 'No Notes')}"
-        cal.events.add(event)
-
     # Create the ICS file content
-    ics_content = cal.serialize()
+    ics_content = get_cal().serialize()
 
     # Base64 encode the ICS content
     encoded_ics_content = base64.b64encode(ics_content.encode('utf-8')).decode('utf-8')
@@ -35,7 +14,8 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'headers': {
             'Content-Type': 'text/calendar',
-            'Content-Disposition': 'attachment; filename="timetable.ics"'
+            'Content-Disposition': 'attachment; filename="timetable.ics"',
+            'Access-Control-Allow-Origin': '*',
         },
         'body': encoded_ics_content,
         'isBase64Encoded': True
--- main.py
+++ main.py
@@ -1,45 +1,66 @@
-# %%
 from datetime import datetime
 
+import arrow
 import requests
-from ics import Calendar, Event
+from ics import Calendar, Event, Organizer
 
-# Fetch JSON data from the URL
-# url = "https://corsi.unibo.it/2cycle/ParticlePhysics/timetable/@@orario_reale_json?anno=2&curricula=&start=2024-09-23&end=2024-09-30"
-# url = "https://corsi.unibo.it/2cycle/ParticlePhysics/timetable/@@orario_reale_json?anno=2"
-url = "https://corsi.unibo.it/2cycle/ParticlePhysics/timetable/@@orario_reale_json?anno=2&curricula=&start=2024-08-01&end=2025-04-01"
-response = requests.get(url)
-data = response.json()
-data
-
-# %%
-len(data)
-
-# %%
-[d['start'] for d in data]
-
-# %%
-# Create a new calendar
-cal = Calendar()
+URL = "https://corsi.unibo.it/2cycle/ParticlePhysics/timetable/@@orario_reale_json?anno=2"
 
 
-# Define a function to convert date strings to datetime objects
-def parse_date(date_str):
-    return datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S")
+def parse_date(date_str) -> arrow.Arrow:
+    # return datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S")
+    #
+    # unaware_datetime = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S")
+    # rome_tz = ZoneInfo('Europe/Rome')
+    # aware_datetime = unaware_datetime.replace(tzinfo=rome_tz)
+    # return aware_datetime
+    #
+    # NOTE: We use Arrow so that our ICS library actually cares about the timezone
+    arrow_date = arrow.get(date_str, tzinfo='Europe/Rome')
+    return arrow_date.to('Europe/Rome')
+    # return arrow.get(date_str).to('Europe/Rome').datetime
 
 
-# Extract event details and create events
-for item in data:
-    event = Event()
-    event.name = item.get('title', 'No Title')
-    event.begin = parse_date(item['start'])
-    event.end = parse_date(item['end'])
-    event.location = item['aule'][0]['des_ubicazione'] if item['aule'] else 'No Location'
-    event.description = f"Professor: {item.get('docente', 'No Professor')}\nNotes: {item.get('note', 'No Notes')}"
-    cal.events.add(event)
+def get_cal():
+    response = requests.get(URL)
+    data = response.json()
 
-# Save the calendar to an ICS file
-with open('timetable.ics', 'w') as f:
-    f.writelines(cal)
+    cal = Calendar(creator="Nicolai Weitkemper, with Unibo data")
 
-print("ICS file generated successfully.")
+    for item in data:
+        event = Event()
+        event.created = datetime.now()  # best thing I can do with a stateless function
+        # event.last_modified = datetime.now()  # best thing I can do with a stateless function
+        event.name = item.get('title', 'No Title')
+        event.begin = parse_date(item['start'])
+        event.end = parse_date(item['end'])
+        aula = item['aule'][0]  # assuming there is always at least one aula
+        # event.location = item['aule'][0]['des_ubicazione'] if item['aule'] else 'No Location'
+        event.location = aula['des_risorsa'] + ", " + aula['des_indirizzo']
+        event.description = f"Professor: {item.get('docente', 'No Professor')}\nNotes: {item.get('note', 'No Notes')}"
+        if 'docente' in item:
+            assert isinstance(item['docente'], str)
+            email_guess = item['docente'].replace(' ', '.').lower() + '@unibo.it'  # TODO: risky
+            event.organizer = Organizer(
+                email=email_guess,
+                common_name=item.get('docente')
+            )
+        if item.get('teams'):
+            assert isinstance(item['teams'], str)
+            event.url = item['teams']
+        cal.events.add(event)
+
+    return cal
+
+
+if __name__ == '__main__':
+    cal = get_cal()
+
+    # Create the ICS file content
+    ics_content = cal.serialize()
+
+    # Save the calendar to an ICS file
+    with open('timetable.ics', 'w') as f:
+        f.writelines(ics_content)
+
+    print("ICS file generated successfully.")
